30e79017059e7bfddee9ff4e95df3243f49b9d50
YonnieHicks <85805865+YonnieHicks@users.noreply.github.com>
2023-03-30 16:05:30 -0500

    All Test Pass

--- queries.sql
+++ queries.sql
@@ -1,23 +1,17 @@
 -- Part 1: Test it with SQL
-
---  id int
---  employer varchar(255)
---  name varchar(255)
---  skills varchar(255)
-
+# id int
+# employer varchar(255)
+# name varchar(255)
+# skills varchar(255)
 
 -- Part 2: Test it with SQL
+-- write a query to list the names of the employers in St. Louis City
+SELECT name FROM employer WHERE location = "St Louis City";
 
---SQL TASK: In queries.sql under “Part 2”,
---write a query to list the names of the employers in St. Louis City.
---Do NOT specify an ordering for the query results.
-
- SELECT name FROM employer WHERE location = "St Louis City";
-
-
+# "SELECT name FROM employer WHERE location = "St. Louis City";"
 -- Part 3: Test it with SQL
 
- DROP TABLE job;
+DROP TABLE job;
 
 -- Part 4: Test it with SQL
 
